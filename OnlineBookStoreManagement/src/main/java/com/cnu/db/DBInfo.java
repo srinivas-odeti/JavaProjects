1718742d133bbbcd7feeb6856f50a0099620a624
srini <srini@srinivas>
2025-03-13 12:56:48 +0530

    Initial commit

---
+++ OnlineBookStoreManagement/src/main/java/com/cnu/db/DBInfo.java
@@ -0,0 +1,9 @@
+package com.cnu.db;
+
+public interface DBInfo 
+{
+	public String driver="oracle.jdbc.driver.OracleDriver";
+	public String dbURL="jdbc:oracle:thin:@localhost:1521:orcl";
+	public String uName="system";
+	public String pWord="tiger";
+}
